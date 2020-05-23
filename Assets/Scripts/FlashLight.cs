3f54a42e66329df0cdfc4ceb7f6e9c17658afe9a
Olivverde19270 <oliver.milian00@gmail.com>
2020-05-23 12:07:13 -0600

    Final commit

---
+++ Assets/Scripts/FlashLight.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashLight : MonoBehaviour
+{
+    private bool isOn = true;
+    private Light emergencyLight;
+    private AudioSource switchSFX;
+
+    void Start()
+    {
+        emergencyLight = GetComponent<Light>();
+        switchSFX = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (emergencyLight && Input.GetMouseButtonDown(0))
+        {
+            isOn = !isOn;
+            emergencyLight.enabled = isOn; 
+            if (switchSFX) switchSFX.Play();
+        }
+    }
+}
---
+++ Assets/Scripts/WeirdLight.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeirdLight : MonoBehaviour
+{
+    private Light emergencyLight;
+    private int intensity = 8;
+    private int frecuency = 4;
+
+    void Start()
+    {
+        emergencyLight = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    private void OnTriggerStay(Collider other)
+    {
+        if (emergencyLight)
+            emergencyLight.intensity = intensity * Mathf.Sin(frecuency * Time.time);
+    }
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (emergencyLight)
+            emergencyLight.intensity = 0;
+    }
+}
